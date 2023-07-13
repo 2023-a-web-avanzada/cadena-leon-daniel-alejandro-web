fe1d159deb164a3ac03d8884e10909450dede2ff
Danrylus <danrylus@gmail.com>
2023-07-13 18:36:14 -0500

    Hasta layouts de react

---
+++ React/my-app/app/a_hola_mundo/page.tsx
@@ -0,0 +1,13 @@
+import C_menuComponent from "../componentes/c_menu_component";
+import DEstilosEjemplo from "../componentes/DEstilosEjemplo";
+
+export default function Home() {
+    return (
+        <>
+            <C_menuComponent />
+            <h1>Hola,desde a_hola_mundo</h1>
+            <DEstilosEjemplo/>
+        </>
+
+    )
+}
